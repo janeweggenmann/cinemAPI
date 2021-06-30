8a2bc69e375a646c391d34ceb83d9e5a8ab11cd8
Jane Weggenmann <janeweggenmann@gmail.com>
2021-06-29 22:42:17 -0400

    Create models.js

---
+++ models.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+let movieSchema = mongoose.Schema({
+  Name: { type: String, required: true },
+  Year: Date,
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+    Birth: Date,
+    Death: Date
+  },
+  ImageURL: String,
+  Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
+});
+
+let Movie = mongoose.model("Movie", movieSchema);
+let User = mongoose.model("User", userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
